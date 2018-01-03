e346aec555ce8268872cfda427d3735738dfdfca
pratikchapagain11 <35002402+pratikchapagain11@users.noreply.github.com>
2018-01-03 09:43:44 +0545

    Add files via upload

---
+++ divisible by 5 or 11 using ternary operator.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int n;
+printf("enter a number");
+scanf("%d",&n);
+(n%5==0||n%11==0)?printf("Number is divisible by 5 and 11"):printf("Number is not divisible by 5 and 11");
+}
